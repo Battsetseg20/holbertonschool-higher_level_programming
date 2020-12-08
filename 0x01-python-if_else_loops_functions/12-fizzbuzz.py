a866f0f2409992938a0da3c33c193d7be4bcce71
Battsetseg20 <tsetseg.yo@gmail.com>
2020-12-08 14:59:56 +0000

    remove comments

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    """Prints numbers from 1 to 100 separated by space.
-    Replace numbers by Fizz, Buzz, or FizzBuzz if conditions met.
-    """
 for number in range (1, 101):
     if number % 15 == 0:
         print("FizzBuzz", end=" ")
