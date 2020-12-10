aabdd8e5fb1d84c6e0f1a74dd9d7cbd891e1224f
Battsetseg20 <tsetseg.yo@gmail.com>
2020-12-10 12:19:59 +0000

    run module as script

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import hidden_4
+if __name__ == "__main__":
+    import hidden_4
 
-defined_names = dir(hidden_4)
-for name in defined_names:
-    if name[:2] != "__":
-        print(name)
+    defined_names = dir(hidden_4)
+    for name in defined_names:
+        if name[:2] != "__":
+            print(name)
