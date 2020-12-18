de0187e137a4b8e8eb1fe0843127e442ee819126
Battsetseg20 <tsetseg.yo@gmail.com>
2020-12-18 12:53:24 +0000

    add if str condition

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,11 +2,12 @@
 
 
 def roman_to_int(roman_string):
-    rom_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M':1000}
-    int_val = 0
-    for i in range(len(roman_string)):
-        if i > 0 and rom_dict[roman_string[i]] > rom_dict[roman_string[i - 1]]:
-            int_val += rom_dict[roman_string[i]] - 2 * rom_dict[roman_string[i -1]]
-        else:
-            int_val += rom_dict[roman_string[i]]
-    return int_val
+    dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    if roman_string and type(roman_string) is str:
+        num = 0
+        for i in range(len(roman_string)):
+            if i > 0 and dict[roman_string[i]] > dict[roman_string[i - 1]]:
+                num += dict[roman_string[i]] - 2 * dict[roman_string[i - 1]]
+            else:
+                num += dict[roman_string[i]]
+        return (num)
