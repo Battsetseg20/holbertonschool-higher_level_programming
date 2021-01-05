40e5a86d98a3ad9ddfed34e2c0b5f411c5dc73e2
Battsetseg20 <tsetseg.yo@gmail.com>
2021-01-05 16:42:01 +0000

    add task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class Square:
+    """Represent a square"""
+
+    def __init__(self, size):
+        """Initialize a private instance attribute to define a square"""
+        self.__size = size
