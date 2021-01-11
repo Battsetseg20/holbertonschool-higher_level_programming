5d96d0904f650bfa5cbc6af62268299bee213b3d
Battsetseg20 <tsetseg.yo@gmail.com>
2021-01-11 19:27:34 +0000

    pep8 checked

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,7 +5,6 @@
 class Rectangle():
     """Represents a rectangle"""
 
-
     def __init__(self, width=0, height=0):
         """Defines a rectangle by width and height"""
         self.width = width
