47caace60b762257f821692c65533c2d52fd4efb
Battsetseg20 <tsetseg.yo@gmail.com>
2021-01-11 19:22:38 +0000

    must add documentation?

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,6 +3,9 @@
 
 
 class Rectangle():
+    """Represents a rectangle"""
+
+
     def __init__(self, width=0, height=0):
         """Defines a rectangle by width and height"""
         self.width = width
@@ -12,16 +15,13 @@ class Rectangle():
     def width(self):
         return self.__width
 
-    @property
-    def height(self):
-        return self.__height
-
     @width.setter
     def width(self, value):
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
+        else:
         self.__width = value
 
     @height.setter
