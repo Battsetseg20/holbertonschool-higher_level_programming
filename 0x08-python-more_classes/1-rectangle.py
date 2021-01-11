b5c93d6b884687a4d6cf0e94f6048745dca5ff41
Battsetseg20 <tsetseg.yo@gmail.com>
2021-01-11 18:54:09 +0000

    set the correct attribute

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -18,20 +18,20 @@ class Rectangle:
         """Set the width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >=0")
         self.__width = value
 
     @property
     def height(self):
         """Get the height attribute"""
-        return self.__width
+        return self.__height
 
     @height.setter
     def height(self, value):
         """Set the height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
