fe3830990cb65ff036addfd31457cbe337180d01
Battsetseg20 <tsetseg.yo@gmail.com>
2021-01-12 19:57:22 +0000

    add task

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,10 +2,11 @@
 class LockedClass:
     """Defines a with no class or object attribtue"""
 
-    # Slots are a nice way to work around this space consumption problem.
-    # Instead of having a dynamic dict that allows adding attributes to
-    # objects dynamically, slots provide a static structure which prohibits
-    # additions after the creation of an instance.
-    # To define slots, you have to define a list with the name __slots__.
-    # The list has to contain all the attributes, you want to use.
+    """Slots are a nice way to work around this space consumption problem.
+    Instead of having a dynamic dict that allows adding attributes to
+    objects dynamically, slots provide a static structure which prohibits
+    additions after the creation of an instance.
+    To define slots, you have to define a list with the name __slots__.
+    The list has to contain all the attributes, you want to use."""
+
     __slots__ = ['first_time']
