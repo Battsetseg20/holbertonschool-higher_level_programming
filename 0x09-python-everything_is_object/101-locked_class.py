fd1e4a81cb3759974d04233f13aa980a0002c05f
Battsetseg20 <tsetseg.yo@gmail.com>
2021-01-12 20:01:23 +0000

    fix documentation

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
+"""class LockedClass with no class or object attribute"""
 
-
-"""Slots are a nice way to work around this space consumption problem.
-Instead of having a dynamic dict that allows adding attributes to
-objects dynamically, slots provide a static structure which prohibits
-additions after the creation of an instance.
-To define slots, you have to define a list with the name __slots__.
-The list has to contain all the attributes, you want to use."""
 
 class LockedClass:
-    """No class or object attrbute"""
-    __slots__ = ['first_time']
+    """Prevents the user from dynamically creating new instance attributes,
+    Except if the new instance attribute is called first_name. """
+    __slots__ = ['first_name']
