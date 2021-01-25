dcbd5090bb5023d80866f25352229a45b46b954f
Battsetseg20 <tsetseg.yo@gmail.com>
2021-01-25 20:51:27 +0000

    valid int first

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -8,7 +8,7 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         """Initializing width and height"""
-        self.__width = width
-        self.__height = height
         self.integer_validator("width", width)
+        self.__width = width
         self.integer_validator("height", height)
+        self.__height = height
