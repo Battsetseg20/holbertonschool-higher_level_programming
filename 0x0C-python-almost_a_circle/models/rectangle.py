26fa49e4b46a2fcc2c1495c6fe72834db40106c9
Battsetseg20 <tsetseg.yo@gmail.com>
2021-01-26 12:34:01 +0000

    call super() first?

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """A subclass Rectangle derived from the Base class"""
-from models.base import Base
+from base import Base
 
 
 class Rectangle(Base):
@@ -8,11 +8,11 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initialize the attributes"""
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        super().__init__(id)
 
         @property
         def width(self):
