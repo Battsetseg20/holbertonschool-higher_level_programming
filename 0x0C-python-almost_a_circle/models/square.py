32a8b2e9e5631f58b40abc75305790d229d4705f
Battsetseg20 <tsetseg.yo@gmail.com>
2021-01-26 15:21:08 +0000

    add task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,7 +11,17 @@ class Square(Rectangle):
         width and height are assigned to the value of size"""
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Gets the size of the Square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+
     def __str__(self):
         """Overloads the Rectangle class behavior by defined method"""
         return "[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width)
+
