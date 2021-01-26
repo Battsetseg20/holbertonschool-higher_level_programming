8bb9c811846d34ea4b2a0f1e5c1b800c4dd1c918
Battsetseg20 <tsetseg.yo@gmail.com>
2021-01-26 12:25:57 +0000

    change condition

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -24,8 +24,8 @@ class Rectangle(Base):
             """Sets the width value"""
             if type(value) is not int:
                 raise TypeError("width must be an integer")
-            elif value < 0:
-                raise ValueError("width must be >= 0")
+            elif value <= 0:
+                raise ValueError("width must be > 0")
             self.__width = value
 
         @property
@@ -38,8 +38,8 @@ class Rectangle(Base):
             """Sets the height value"""
             if type(value) is not int:
                 raise TypeError("height must be an integer")
-            elif value < 0:
-                raise ValueError("height must be >= 0")
+            elif value <= 0:
+                raise ValueError("height must be > 0")
             self.__height = value
 
         @property
