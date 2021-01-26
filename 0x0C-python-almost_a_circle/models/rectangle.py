c8225ce6e928ba24a4da21fca8de488ce13659c1
Battsetseg20 <tsetseg.yo@gmail.com>
2021-01-26 12:30:05 +0000

    test without condition

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -22,10 +22,6 @@ class Rectangle(Base):
         @width.setter
         def width(self, value):
             """Sets the width value"""
-            if type(value) is not int:
-                raise TypeError("width must be an integer")
-            elif value <= 0:
-                raise ValueError("width must be > 0")
             self.__width = value
 
         @property
@@ -36,10 +32,6 @@ class Rectangle(Base):
         @height.setter
         def height(self, value):
             """Sets the height value"""
-            if type(value) is not int:
-                raise TypeError("height must be an integer")
-            elif value <= 0:
-                raise ValueError("height must be > 0")
             self.__height = value
 
         @property
@@ -50,10 +42,6 @@ class Rectangle(Base):
         @x.setter
         def x(self, value):
             """Sets the value of x"""
-            if type(value) is not int:
-                raise TypeError("x must be an integer")
-            if value < 0:
-                raise ValueError("x must be >= 0")
             self.__x = value
 
         @property
@@ -64,8 +52,4 @@ class Rectangle(Base):
         @y.setter
         def y(self, value):
             """Sets the value of y"""
-            if type(value) != int:
-                raise TypeError("y must be an integer")
-            if value < 0:
-                raise ValueError("y must be >= 0")
             self.__y = value
