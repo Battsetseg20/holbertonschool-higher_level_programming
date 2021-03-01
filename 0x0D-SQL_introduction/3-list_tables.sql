5de83d872e444afc619a0844daf2fea300bcbe70
Battsetseg20 <tsetseg.yo@gmail.com>
2021-03-01 16:06:40 +0000

    add files

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,4 @@
+-- lists all the tables of a database in your MySQL server.
+-- The database name will be passed as argument of mysql command
+-- SHOW TABLES /  mysqlshow db_name  lists the non-TEMP tables in given database
+SHOW TABLES
