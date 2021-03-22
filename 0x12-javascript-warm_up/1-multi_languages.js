efead797cb63548bddea77ef3febe1647b0e2a39
Battsetseg20 <tsetseg.yo@gmail.com>
2021-03-22 15:55:53 +0000

    fix task 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,4 @@
 #!/usr/bin/node
-const myLines = 'C is fun\nPython is cool\nJavascript is amazing';
-console.log(myLines);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
