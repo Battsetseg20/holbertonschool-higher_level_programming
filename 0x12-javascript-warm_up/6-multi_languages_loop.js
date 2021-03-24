306e4726b84512d4b5162a1b13a881a73f57b6fb
Battsetseg20 <tsetseg.yo@gmail.com>
2021-03-24 08:52:20 +0000

    test code huy

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
-for (const myArray of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
-  console.log(myArray);
+// Prints 3 lines: by using an array of string and a loop
+
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const l of lines) {
+  console.log(l);
 }
