a08389af5af1c7bff4e6ea784d7cb6525a1e95d6
Battsetseg20 <tsetseg.yo@gmail.com>
2021-03-28 18:40:50 +0000

    make query to compare a value with exact case sensitive match

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("SELECT * \
     FROM states \
-    WHERE name = '{}'\
+    WHERE BINARY name = '{}'\
     ORDER BY id ASC".format(argv[4]))
     [print(state) for state in cur.fetchall()]
     cur.close()
